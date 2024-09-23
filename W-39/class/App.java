bf4ee50cfe85df27945c4b57949f6b0e5a4f3678
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-09-23 11:42:01 +0300

    while loop

---
+++ W-39/class/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java is working");
+
+        int i = 0;
+
+        while (i < 8) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
