2de0cae9856feae606ccf4486a4a374ef0e6df0c
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-09-23 11:45:57 +0300

    while loop (decrease)

---
+++ W-39/class/Decrease.java
@@ -0,0 +1,12 @@
+public class Decrease {
+    public static void main(String[] args) {
+        System.out.println("Java is working");
+
+        int i = 8;
+
+        while (i >= 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
